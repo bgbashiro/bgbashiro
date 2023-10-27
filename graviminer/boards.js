25ea87d40577af020813d56b189327884c81c133
Bashirov <bgbashirov@gmail.com>
2023-10-27 15:32:26 +0100

    init halloffame

---
+++ graviminer/boards.js
@@ -0,0 +1,15 @@
+
+function updateLeaderBoard(level) {
+    let leaderboardOL = document.querySelector("#leaderboard > ol")
+    leaderboardOL.innerHTML = "";
+    fetchLeaderBoard(level).then(rows => {
+        Object.entries(rows).forEach(([uuid, score]) => {
+            getUsername(uuid).then(row => {
+                let li = document.createElement("li")
+                li.innerText = `${row} : ${score}`
+                li.classList.add('leaderboard-entry');
+                leaderboardOL.appendChild(li);
+            })
+        })
+    })
+}
